eaac1d0621843c356a4a8e2db4b1069604ad312c
Danny-ot <oladejidaniel456@gmail.com>
2022-10-04 04:18:14 -0700

    create database and connect it mysql

---
+++ TravelApi/Migrations/20220819130101_Initial.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TravelApi.Solution.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Destination",
+                columns: table => new
+                {
+                    DestinationId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Username = table.Column<string>(type: "varchar(15) CHARACTER SET utf8mb4", maxLength: 15, nullable: false),
+                    Country = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: false),
+                    City = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: false),
+                    DestinationName = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: false),
+                    Review = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Destination", x => x.DestinationId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Destination",
+                columns: new[] { "DestinationId", "City", "Country", "DestinationName", "Review", "Username" },
+                values: new object[] { 1, "Lagos", "Nigeria", "EmmanuelPark", "Emmanurl park is one of the best restaurants in nigeria", "Danny" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Destination");
+        }
+    }
+}
---
+++ TravelApi/Migrations/20220823114509_ModifyDestinationTable.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TravelApi.Solution.Migrations
+{
+    public partial class ModifyDestinationTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "OverallRating",
+                table: "Destination",
+                type: "double",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OverallRating",
+                table: "Destination");
+        }
+    }
+}
